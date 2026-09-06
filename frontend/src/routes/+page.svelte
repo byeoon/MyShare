89140f3203157114807ab45af2ca41db9868efa2
byeoon <byeoon@byeoon.dev>
2026-09-06 19:21:00 -0400

    feat: ShareX Upload support

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,35 @@
     import Sidebar from '$lib/components/Sidebar.svelte';
     import DashboardTabs from '$lib/components/DashboardTabs.svelte';
     import CreateNote from '$lib/components/CreateNote.svelte';
+
+    // TODO: move to an asset folder so it is directly accessable, if possible
+    function downloadShareXConfig() {
+        const token = localStorage.getItem('token');
+        const origin = window.location.origin;
+
+        const config = {
+            Version: '15.0.0',
+            Name: 'MyShare',
+            DestinationType: 'ImageUploader, TextUploader, FileUploader',
+            RequestMethod: 'POST',
+            RequestURL: `${origin}/api/upload`,
+            Headers: {
+                Authorization: token,
+            },
+            Body: 'MultipartFormData',
+            FileFormName: 'file',
+            URL: `${origin}/uploads/{json:filename}`,
+            ErrorMessage: '{json:error}',
+        };
+
+        const blob = new Blob([JSON.stringify(config, null, 2)], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'myshare.sxcu';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
 </script>
 
 <Sidebar>
