c821464e941a00b9a6ca52fbd9019e8caecef0db
Your Name <47872200+byeoon@users.noreply.github.com>
2026-09-05 20:58:47 -0400

    what(idk): added server proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
                 runes: ({ filename }) =>
                     filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
             },
+            server: {
+                proxy: {
+                    '/api': 'http://localhost:8000'
+                }
+            }
 
             // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
             // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
