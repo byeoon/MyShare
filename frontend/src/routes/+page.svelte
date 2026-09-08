7c586395a4bc1a62d9a2a36a28c46d4d59e3e09e
byeoon <byeoon@byeoon.dev>
2026-09-07 22:03:45 -0400

    refactor: rename DashboardContainer to DashboardTabs

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
     import Sidebar from '$lib/components/Sidebar.svelte';
-    import DashboardContainer from '$lib/components/DashboardContainer.svelte';
+    import DashboardTabs from '$lib/components/DashboardTabs.svelte';
     import CreateNote from '$lib/components/CreateNote.svelte';
 </script>
 
 <Sidebar>
     <div class="flex flex-col items-center justify-start w-full py-4">
-        <p class="text-lg font-bold">
-            This is text before the dashboard container. Feel free to add notifications here, or
-            something.
-        </p>
-        <DashboardContainer />
+        <p class="text-lg font-bold">Welcome to the dashboard!</p>
+        <DashboardTabs />
     </div>
     <CreateNote />
 </Sidebar>
