9cfa0783707142e520fc8bcfe3aa5767adaef1d0
byeoon <byeoon@byeoon.dev>
2026-09-07 21:41:53 -0400

    I HAVE NO IDEA WHAT THIS IS, I HATE CODING WITHOUT A SHIFT KEY, PLEASE HELP, IDK IF THIS IS BREAKING, FUCK THIS KEYBOARD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        sveltekit({
-            // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-            // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-            // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-            adapter: adapter(),
-        }),
+        sveltekit(),
     ],
     server: {
         watch: {
             usePolling: true,
+            ignored: ['**/.svelte-kit/**', '**/.pnpm-store/**'],
         },
         proxy: {
             '/api': {
