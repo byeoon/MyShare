ea536d356f844bf68e2c81aeb93209d3fd4200c9
byeoon <byeoon@byeoon.dev>
2026-09-08 12:28:27 -0400

    feat: added implementation for notes page

---
+++ frontend/src/lib/components/Skeleton.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div class="skeleton h-32 w-32"></div>
---
+++ frontend/src/routes/note/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { page } from '$app/state';
+    import Skeleton from '$lib/components/Skeleton.svelte';
+</script>
+
+<p>Random error. please fix.</p>
