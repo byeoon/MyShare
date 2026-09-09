68bc490e3da875ed478903d05704bad1652df2a1
byeoon <byeoon@byeoon.dev>
2026-09-08 22:01:26 -0400

    fix(frontend): add proper modal showing

--- frontend/src/routes/Editor.svelte
+++ frontend/src/routes/Editor.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-
 </script>
 
-<div class="fixed z-100 inset-0 bg-black/30 flex items-center justify-center p-3">
-    <div class="w-full h-full">
-        <div class="flex flex-col items-center justify-center h-full">
-        </div>
+<div
+    class="fixed inset-0 z-50 bg-black/40 backdrop-blur-sm flex items-center justify-center p-4 sm:p-6"
+>
+    <div
+        class="bg-base-100/90 backdrop-blur-2xl border border-base-content/10 shadow-2xl rounded-2xl w-full max-w-3xl h-[80vh] max-h-[750px] flex flex-col overflow-hidden relative"
+    >
+        <p>hi, create a note placeholder modal, will add x button and pres to close soon</p>
     </div>
 </div>
