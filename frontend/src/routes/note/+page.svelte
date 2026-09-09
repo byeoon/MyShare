5a4a6ff748485306e877fb8c1a7dc4e987520371
Laura <laura@pre1ude.dev>
2026-09-09 11:52:11 +0200

    chore(format): format code

--- frontend/src/routes/note/+page.svelte
+++ frontend/src/routes/note/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     // TODO: Use [slug] directory (/notes/[slug]) to display a note based on its slug (id)
+    // https://svelte.dev/docs/kit/routing#:~:text=src%2Froutes%2Fblog%2F%5Bslug
 </script>
 
 <p>Unused page</p>
